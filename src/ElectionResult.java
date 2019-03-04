c0d5efe768050674bfa9a191609f19bb9daa6a00
Roshan Prabhakar <roshan@aprabhakar.com>
2019-03-03 18:09:49 -0800

    finished 2

--- src/ElectionResult.java
+++ src/ElectionResult.java
@@ -90,4 +90,21 @@ public class ElectionResult {
         this.combined_fips = combined_fips;
     }
 
+    public ElectionResult(int votes_dem, int votes_gop, int total_votes, double per_dem, double per_gop, String diff, double perPointDiff, String state, String county_name, int combined_fips) {
+        this.votes_dem = votes_dem;
+        this.votes_gop = votes_gop;
+        this.total_votes = total_votes;
+        this.per_dem = per_dem;
+        this.per_gop = per_gop;
+        this.diff = diff;
+        this.perPointDiff = perPointDiff;
+        this.state = state;
+        this.county_name = county_name;
+        this.combined_fips = combined_fips;
+    }
+
+    public String toString() {
+        return votes_dem + "," + votes_gop + "," + total_votes + "," + per_dem + "," + per_gop + "," + diff + "," + perPointDiff + "," + state + "," + county_name + "," + combined_fips;
+    }
+
 }
--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -19,4 +20,18 @@ public class Utils {
 
         return output.toString();
     }
+
+    public static ArrayList<ElectionResult> parse2016PresidentialResults() {
+        String resultsFile = readFileAsString("data" + File.separator + "2016_Presidential_Results.csv");
+        String[] data = resultsFile.split("\n");
+        ArrayList<ElectionResult> dataList = new ArrayList<>();
+        for (int i = 1; i < data.length; i++) {
+            String[] scores = data[i].split(",");
+            dataList.add(new ElectionResult(
+               Integer.parseInt(scores[1]), Integer.parseInt(scores[2]), Integer.parseInt(scores[3]), Double.parseDouble(scores[4]), Double.parseDouble(scores[5]), scores[6], Double.parseDouble(scores[7]), scores[8], scores[9], Integer.parseInt(scores[10])
+            ));
+        }
+        return null;
+    }
+
 }
